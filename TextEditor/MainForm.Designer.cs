4496f2aed741ca9300683e8384e9386a36f2e24a
Константин Груздев <vanilmirth@inbox.ru>
2023-08-04 14:28:07 +0300

    25:19

--- TextEditor/MainForm.Designer.cs
+++ TextEditor/MainForm.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbChooseFile = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnOpenfile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numFont = new System.Windows.Forms.NumericUpDown();
+            this.tbContent = new System.Windows.Forms.TextBox();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lbSymbolCount = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите файл";
+            // 
+            // tbChooseFile
+            // 
+            this.tbChooseFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbChooseFile.Location = new System.Drawing.Point(152, 23);
+            this.tbChooseFile.Name = "tbChooseFile";
+            this.tbChooseFile.Size = new System.Drawing.Size(715, 20);
+            this.tbChooseFile.TabIndex = 1;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFile.Location = new System.Drawing.Point(892, 21);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Выбрать";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenfile
+            // 
+            this.btnOpenfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenfile.Location = new System.Drawing.Point(973, 21);
+            this.btnOpenfile.Name = "btnOpenfile";
+            this.btnOpenfile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenfile.TabIndex = 3;
+            this.btnOpenfile.Text = "Открыть";
+            this.btnOpenfile.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шрифт";
+            // 
+            // numFont
+            // 
+            this.numFont.Location = new System.Drawing.Point(152, 67);
+            this.numFont.Maximum = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            this.numFont.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numFont.Name = "numFont";
+            this.numFont.Size = new System.Drawing.Size(120, 20);
+            this.numFont.TabIndex = 5;
+            this.numFont.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // tbContent
+            // 
+            this.tbContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbContent.Location = new System.Drawing.Point(39, 108);
+            this.tbContent.Multiline = true;
+            this.tbContent.Name = "tbContent";
+            this.tbContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbContent.Size = new System.Drawing.Size(1009, 448);
+            this.tbContent.TabIndex = 6;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveFile.Location = new System.Drawing.Point(973, 575);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFile.TabIndex = 7;
+            this.btnSaveFile.Text = "Сохранить";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lbSymbolCount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 625);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1080, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(133, 17);
+            this.toolStripStatusLabel1.Text = "Количество символов:";
+            // 
+            // lbSymbolCount
+            // 
+            this.lbSymbolCount.Name = "lbSymbolCount";
+            this.lbSymbolCount.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1080, 647);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.tbContent);
+            this.Controls.Add(this.numFont);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOpenfile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.tbChooseFile);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Текстовый редактор";
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbChooseFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnOpenfile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numFont;
+        private System.Windows.Forms.TextBox tbContent;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lbSymbolCount;
     }
 }
 
--- TextEditor/MainForm.cs
+++ TextEditor/MainForm.cs
@@ -16,5 +16,6 @@ namespace TextEditor
         {
             InitializeComponent();
         }
+               
     }
 }
